c267b60226687d8956e2be920f2a66d13966f2fa
Anttwork <345183106@qq.com>
2023-06-05 17:50:09 +0800

    编写详情页面头部样式

--- app/src/main/java/com/ui/ApkSteady/ui/DetailActivity.java
+++ app/src/main/java/com/ui/ApkSteady/ui/DetailActivity.java
@@ -9,6 +9,7 @@ public class DetailActivity extends BaseCommonActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        setContentView(R.layout.my_fragment);
         setContentView(R.layout.match_detail_activity);
     }
 }
