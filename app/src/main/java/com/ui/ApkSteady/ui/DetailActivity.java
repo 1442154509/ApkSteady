d316720a758f2547f0c4e2094d04fc22fdd98b2f
Anttwork <345183106@qq.com>
2023-06-06 10:58:51 +0800

    首页顶部tab修改， 加入底部导航关注切换图标， 详情页面头部编写

--- app/src/main/java/com/ui/ApkSteady/ui/DetailActivity.java
+++ app/src/main/java/com/ui/ApkSteady/ui/DetailActivity.java
@@ -1,15 +1,27 @@
 package com.ui.ApkSteady.ui;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.ui.ApkSteady.R;
 
 public class DetailActivity extends BaseCommonActivity {
+
+    private ImageView mBackIv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.my_fragment);
         setContentView(R.layout.match_detail_activity);
+
+        mBackIv = findViewById(R.id.imageView_detail_back);
+        mBackIv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
